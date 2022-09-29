5f0dc2491d4b993b6c40ff57d2c659d629150c24
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-29 21:03:10 +0200

    Add public methods to RoboRunTable

--- RoboRun/RoboRun.Persistence/RoboRunTable.cs
+++ RoboRun/RoboRun.Persistence/RoboRunTable.cs
@@ -12,7 +12,7 @@
 
         #endregion
 
-        #region Constructor
+        #region Constructors
 
         /// <summary>
         /// Instantiate RoboRunTable.
@@ -30,6 +30,30 @@
 
             _fieldValues = new int[tableSize, tableSize];
             _fieldLocks = new bool[tableSize, tableSize];
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public bool HasWall(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsLocked(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLock(int x, int y)
+        {
+            if (x < 0 || x >= _fieldValues.GetLength(0))
+                throw new ArgumentOutOfRangeException("x", "The X coordinate is out of range.");
+            if (y < 0 || y >= _fieldValues.GetLength(1))
+                throw new ArgumentOutOfRangeException("y", "The Y coordinate is out of range.");
+
+            _fieldLocks[x, y] = true;
         }
 
         #endregion
