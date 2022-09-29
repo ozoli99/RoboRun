3dba85f14eb7adf41a6ef5dadc5f471931fa15db
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-29 20:16:41 +0200

    Add private fields to GameForm

--- RoboRun/RoboRun.View/GameForm.cs
+++ RoboRun/RoboRun.View/GameForm.cs
@@ -2,9 +2,22 @@ namespace RoboRun.View
 {
     public partial class GameForm : Form
     {
+        #region Private fields
+
+        private IRoboRunDataAccess _dataAccess; // persistence
+        private RoboRunModel _model;    // game model
+        private Button[,] _buttonGrid;
+        private System.Windows.Forms.Timer _timer;
+
+        #endregion
+
+        #region Constructor
+
         public GameForm()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
