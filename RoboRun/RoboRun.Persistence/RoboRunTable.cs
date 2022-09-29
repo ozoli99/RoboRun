9ec48707b052ce51eb7806b6a401353b4a133eda
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-29 20:58:40 +0200

    Implement RoboRunTable constructors

--- RoboRun/RoboRun.Persistence/RoboRunTable.cs
+++ RoboRun/RoboRun.Persistence/RoboRunTable.cs
@@ -14,9 +14,22 @@
 
         #region Constructor
 
-        public RoboRunTable()
-        {
+        /// <summary>
+        /// Instantiate RoboRunTable.
+        /// </summary>
+        public RoboRunTable() : this(11) { }
 
+        /// <summary>
+        /// Instantiate RoboRunTable.
+        /// </summary>
+        /// <param name="tableSize">Size of game table.</param>
+        public RoboRunTable(int tableSize)
+        {
+            if (tableSize < 0)
+                throw new ArgumentOutOfRangeException("The table size is less than 0.", "tableSize");
+
+            _fieldValues = new int[tableSize, tableSize];
+            _fieldLocks = new bool[tableSize, tableSize];
         }
 
         #endregion
