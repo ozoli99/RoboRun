b99a009f43e17df583ff9bca3c586e06575569ec
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-29 20:37:58 +0200

    Add private fields and properties to RoboRunModel

--- RoboRun/RoboRun.Model/RoboRunModel.cs
+++ RoboRun/RoboRun.Model/RoboRunModel.cs
@@ -5,6 +5,28 @@
     /// </summary>
     public class RoboRunModel
     {
+        #region Private fields
 
+        private IRoboRunDataAccess _dataAccess; // persistence
+        private RoboRunTable _gameTable;
+        private int _gameTime;
+
+        #endregion
+
+        #region Properties
+
+        public RoboRunTable GameTable { get { return _gameTable; } }
+        public int GameTime { get { return _gameTime; } }
+
+        #endregion
+
+        #region Constructor
+
+        public RoboRunModel()
+        {
+
+        }
+
+        #endregion
     }
 }
