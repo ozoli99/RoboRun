3c1a46d8c514ce12f159e668018075552a0e76f7
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-29 20:54:21 +0200

    Add Persistence project to solution and create IRoboRunDataAccess interface and RoboRunTable class

---
+++ RoboRun/RoboRun.Persistence/IRoboRunDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace RoboRun.Persistence
+{
+    public interface IRoboRunDataAccess
+    {
+        RoboRunTable Load(string path);
+        void Save(string path, RoboRunTable gameTable, int gameTime);
+    }
+}
---
+++ RoboRun/RoboRun.Persistence/RoboRunTable.cs
@@ -0,0 +1,24 @@
+﻿namespace RoboRun.Persistence
+{
+    /// <summary>
+    /// Type of RoboRun game table.
+    /// </summary>
+    public class RoboRunTable
+    {
+        #region Private fields
+
+        private int[,] _fieldValues;
+        private bool[,] _fieldLocks;
+
+        #endregion
+
+        #region Constructor
+
+        public RoboRunTable()
+        {
+
+        }
+
+        #endregion
+    }
+}
