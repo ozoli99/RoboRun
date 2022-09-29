68500e9eb74390e49623381045d28ca6104e6ed1
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-29 23:27:37 +0200

    Create RoboRunFileDAtaAccess that implements IRoboRunDataAccess interface

---
+++ RoboRun/RoboRun.Persistence/RoboRunFileDataAccess.cs
@@ -0,0 +1,15 @@
+﻿namespace RoboRun.Persistence
+{
+    public class RoboRunFileDataAccess : IRoboRunDataAccess
+    {
+        public RoboRunTable Load(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string path, RoboRunTable gameTable, int gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
