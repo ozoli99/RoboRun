876b2a4509ac34d784de5f46ebe4cf95984b12b3
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-01 14:55:49 +0200

    Add and implement Robot class

---
+++ RoboRun/RoboRun.Persistence/Robot.cs
@@ -0,0 +1,16 @@
+﻿namespace RoboRun.Persistence
+{
+    public class Robot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool ReachedHome { get; set; }
+
+        public Robot(int x, int y, bool reachedHome)
+        {
+            X = x;
+            Y = y;
+            ReachedHome = reachedHome;
+        }
+    }
+}
