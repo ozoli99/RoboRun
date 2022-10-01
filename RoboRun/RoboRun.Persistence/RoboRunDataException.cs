9042bc4410603b09542eb05660cbff7ff4d850b8
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-01 18:26:13 +0200

    Add and implement RoboRunDataException to Persistence

---
+++ RoboRun/RoboRun.Persistence/RoboRunDataException.cs
@@ -0,0 +1,13 @@
+﻿namespace RoboRun.Persistence
+{
+    /// <summary>
+    /// Type of RoboRun data exception.
+    /// </summary>
+    public class RoboRunDataException : Exception
+    {
+        /// <summary>
+        /// Instantiate RoboRun data exception.
+        /// </summary>
+        public RoboRunDataException() { }
+    }
+}
