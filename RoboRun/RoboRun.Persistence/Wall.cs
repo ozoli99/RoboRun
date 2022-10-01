499bd04824f89342becf2febcba72564e045c371
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-01 14:57:29 +0200

    Add and implement Wall class

---
+++ RoboRun/RoboRun.Persistence/Wall.cs
@@ -0,0 +1,16 @@
+﻿namespace RoboRun.Persistence
+{
+    public class Wall
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Collapsed { get; set; }
+
+        public Wall(int x, int y, bool collapsed)
+        {
+            X = x;
+            Y = y;
+            Collapsed = collapsed;
+        }
+    }
+}
