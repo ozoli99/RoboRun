197b585ed06cb755d41dceae32d70489474e97a5
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-03 18:09:52 +0200

    Implement other test methods

--- RoboRun/RoboRunTest/TestModel.cs
+++ RoboRun/RoboRunTest/TestModel.cs
@@ -20,13 +20,12 @@ namespace RoboRunTest
             _mock.Setup(mock => mock.LoadAsync(It.IsAny<string>())).Returns(() => Task.FromResult(_mockedTable));
 
             _model = new RoboRunModel(_mock.Object);
-            //_model.GameWin += new EventHandler<RoboRunEventArgs>(Model_GameWin);
-            //_model.GameTimeAdvanced += new EventHandler<RoboRunEventArgs>(Model_GameTimeAdvanced);
-            //_model.GameTimePaused += new EventHandler<RoboRunEventArgs>(Model_GameTimePaused);
-            //_model.RobotMoved += new EventHandler(Model_RobotMoved);
+            _model.GameWin += new EventHandler<RoboRunEventArgs>(Model_GameWin);
+            _model.GameTimeAdvanced += new EventHandler<RoboRunEventArgs>(Model_GameTimeAdvanced);
+            _model.GameTimePaused += new EventHandler<RoboRunEventArgs>(Model_GameTimePaused);
         }
 
-        #region Load
+        #region Public methods tests
 
         [TestMethod]
         public void RoboRunModelNewGameSmallTest()
@@ -134,6 +133,72 @@ namespace RoboRunTest
 
             Assert.AreEqual(1, _model.GameTable.Walls.Count);
             Assert.AreEqual(true, _model.GameTable.IsLocked(2, 8));
+        }
+
+        [TestMethod]
+        public void RoboRunModelAdvanceTimeTest()
+        {
+            _model.NewGame(2, 5, Direction.Down);
+
+            Assert.AreEqual(0, _model.GameTime);
+        }
+
+        #endregion
+
+        #region Event tests
+
+        private void Model_GameTimeAdvanced(object sender, RoboRunEventArgs e)
+        {
+            Assert.IsTrue(_model.GameTime >= 0);
+
+            Assert.AreEqual(e.ElapsedTime, _model.GameTime);
+        }
+
+        private void Model_GameTimePaused(object sender, RoboRunEventArgs e)
+        {
+            Assert.IsTrue(_model.GameTime >= 0);
+
+            Assert.AreEqual(e.ElapsedTime, _model.GameTime);
+        }
+
+        private void Model_GameWin(object sender, RoboRunEventArgs e)
+        {
+            Assert.IsTrue(_model.IsGameWin);
+            Assert.AreEqual(e.ElapsedTime, _model.GameTime);
+            Assert.AreEqual(_model.GameTable.Robot.X, _model.GameTable.Size / 2);
+            Assert.AreEqual(_model.GameTable.Robot.Y, _model.GameTable.Size / 2);
+        }
+
+        #endregion
+
+        #region Async methods tests
+
+        [TestMethod]
+        public async Task RoboRunModelLoadTest()
+        {
+            _model.NewGame(2, 8, Direction.Down);
+
+            await _model.LoadGameAsync(string.Empty);
+
+            for (int i = 0; i < _model.GameTable.Size; i++)
+            {
+                for (int j = 0; j < _model.GameTable.Size; j++)
+                {
+                    Assert.AreEqual(_mockedTable.IsLocked(i, j), _model.GameTable.IsLocked(i, j));
+                    Assert.AreEqual(_mockedTable.HasWall(i, j), _model.GameTable.HasWall(i, j));
+
+                    if (i == _model.GameTable.Size / 2 && j == _model.GameTable.Size / 2)
+                    {
+                        Assert.AreEqual(_mockedTable.IsHome(i, j), _model.GameTable.IsHome(i, j));
+                    }
+                    if (i == _model.GameTable.Robot.X && j == _model.GameTable.Robot.Y)
+                    {
+                        Assert.AreEqual(_mockedTable.IsRobot(i, j), _model.GameTable.IsRobot(i, j));
+                    }
+                }
+            }
+
+            _mock.Verify(dataAccess => dataAccess.LoadAsync(string.Empty), Times.Once());
         }
 
         #endregion
